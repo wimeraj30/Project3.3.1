cbdc0aef524ef1ce725c8dc1ad44a6969dc6f58d
Alton Wimer <98047098+wimeraj30@users.noreply.github.com>
2022-03-29 16:53:02 +0000

    Added Login

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-a = 'I would like to make a change.'
-print("hello")
+import db
+
+start = input('New user: (N)\nReturning user: (R)\n')
+if start == 'N':
+  nusr = input("Username: ")
+  npsw = input("Password: ")
+  db[str(nusr)] = nusr, npsw
+elif start == 'R':
+  rusr = input("Username: ")
+  rpsw = input("Password: ")
+  if rusr in db.keys():
+    if rpsw in db[rusr]:
+      print("Logged in")
