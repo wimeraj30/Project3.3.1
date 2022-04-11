587456966406f49b81792abeda71723033b1f693
Alton Wimer <98047098+wimeraj30@users.noreply.github.com>
2022-04-11 16:36:49 +0000

    New to-do list save function.

--- main.py
+++ main.py
@@ -5,17 +5,20 @@ import todo
 import schedule
 to_do_list= []
 
+rusr = ''
 
 def login():
+  global rusr
   start = input('New user: (N)\nReturning user: (R)\n')
   if start == 'N' or start == 'n':
     nusr = input("Username: ")
     npsw = input("Password: ")
     db[str(nusr)] = nusr, npsw
+    rusr = nusr
     print("Account Created")
     time.sleep(1)
     os.system("clear")
-    mainmenu(to_do_list)
+    mainmenu(to_do_list, rusr)
   elif start == 'R' or start == 'r':
     rusr = input("Username: ")
     rpsw = input("Password: ")
@@ -24,7 +27,7 @@ def login():
         print("Logged in")
         time.sleep(1)
         os.system("clear")
-        mainmenu(to_do_list)
+        mainmenu(to_do_list, rusr)
       else:
         print("Wrong Password")
         rpsw = input("Password: ")
@@ -32,7 +35,7 @@ def login():
           print("Logged in")
           time.sleep(1)
           os.system("clear")
-          mainmenu(to_do_list)
+          mainmenu(to_do_list, rusr)
         else:
           print("Wrong Password")
           time.sleep(2)
@@ -45,7 +48,7 @@ def login():
       login()
 
 
-def mainmenu(to_do_list):
+def mainmenu(to_do_list, rusr):
   choice = input('* * * * * * * * * * * * * * * *\n'
                '*                             *\n'
                '*   Welcome!                  *\n'
@@ -54,6 +57,7 @@ def mainmenu(to_do_list):
                '*   from the choices below    *\n'
                '*                             *\n'
                "*   ('T') To-do list          *\n"
+               "*   ('C') Calendar            *\n"
                "*   ('S') Schedule            *\n"
                "*   ('L') Logout              *\n"
                '*                             *\n'
@@ -61,20 +65,20 @@ def mainmenu(to_do_list):
   if choice == 'T' or choice == 't':
     os.system("clear")
     print("To do list")
-    to_do_list = todo.to_do()
+    to_do_list = todo.to_do(rusr)
     os.system('clear')
-    mainmenu(to_do_list)
+    mainmenu(to_do_list, rusr)
+  if choice == 'C' or choice == 'c':
+    os.system("clear")
   if choice == 'S' or choice == 's':
     os.system("clear")
     print("Schedule")
     to_do_list = schedule.settime(to_do_list)
-    mainmenu(to_do_list)
+    mainmenu(to_do_list, rusr)
   if choice == 'L' or choice == 'l':
     os.system("clear")
     login()
   
-
-
 login()
 
 
--- todo.py
+++ todo.py
@@ -1,28 +1,44 @@
 import os
+from replit import db
 
 to_do_list=[]
 
-def to_do():
+def to_do(rusr):
+  to_do_list = []
+  namedat = str(rusr) + " To-Do"
+  if namedat in db.keys():
+    to_do_list = db[str(namedat)]
+  else:
+    db[str(namedat)] = []
+    to_do_list = db[str(namedat)]
   to_do_action= ''
   while to_do_action != 'done':
-    print(to_do_list)
+    cut = str(to_do_list)
+    if 'ObservedList(value=' in cut:
+      cut = cut[19:-1]
+    print(cut)
     to_do_action= input("What would you like to do with your to-do list? Add or remove items? (type 'add' or 'remove') \n If finished, type 'done'. ")
     os.system("clear")
     if to_do_action == "add":
       added_subject = ''
       while added_subject != "done":
-        print(to_do_list)
+        print(cut)
         added_subject = input("What subject would you like to add to your to-do list? \nIf finished adding items to your to-do list then type (done) ")
         os.system('clear')
         to_do_list.append(added_subject)
+        cut = str(to_do_list)
+        cut = cut[19:-1]
       del to_do_list[-1]
     elif to_do_action == "remove":
       removed_subject= ''
       while removed_subject!= 'done':
-        print(to_do_list)
+        print(cut)
         removed_subject = input("What subject would you like to remove? \n(Type as appears in list). Type 'done' when done. ")
         os.system('clear')
         if removed_subject != 'done':
           to_do_list.remove(removed_subject)
   print("Your to-do list is as follows: "+ str(to_do_list))
+  namedat = str(rusr) + " To-Do"
+  db[namedat] = to_do_list
+
   return to_do_list
